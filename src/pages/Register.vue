4055d4d4a294b1556d66893c0aad92f4dee69dd4
Do Minh Hai <haianhnc@gmail.com>
2020-02-16 04:59:39 +0700

    [Add] validation & refactoring

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -7,9 +7,13 @@
               <div class="field">
                 <label class="label title-s1">Your Account Information</label>
               </div>
-              <div class="columns">
+              <div v-show="!isEditing" class="field has-text-centered">
+                {{ first_name }} {{ last_name }}
+              </div>
+              <div v-show="isEditing" class="columns">
                 <div class="column field">
                   <div class="control">
+                    <ValidationProvider v-slot="v" rules="required">
                       <input
                         class="input"
                         type="text"
@@ -17,24 +21,29 @@
                         v-model="first_name"
                         :disabled="!isEditing"
                       >
+                    <p class="help is-danger">{{ v.errors[0] }}</p>
+                    </ValidationProvider>
                   </div>
                 </div>
                 <div class="column field">
                   <div class="control">
+                    <ValidationProvider v-slot="v" rules="required">
                       <input
                         class="input"
                         type="text" placeholder="Last Name"
                         v-model="last_name"
                         :disabled="!isEditing"
                       >
+                      <p class="help is-danger">{{ v.errors[0] }}</p>
+                    </ValidationProvider>
                   </div>
                 </div>
               </div>
               <div class="field">
-                <!-- <label class="label">Email</label> -->
                 <div class="control has-icons-left has-icons-right">
+                  <ValidationProvider v-slot="v" rules="required">
                     <input
-                      class="input is-danger"
+                      class="input"
                       type="email"
                       placeholder="Email input"
                       v-model="email"
@@ -46,41 +55,56 @@
                     <span class="icon is-small is-right">
                     <i class="fa fa-warning"></i>
                     </span>
+                    <p class="help is-danger">{{ v.errors[0] }}</p>
+                  </ValidationProvider>
                 </div>
-                <p class="help is-danger">This email is invalid</p>
               </div>
               <div class="field">
-                <p class="control has-icons-left">
-                  <input
-                    class="input"
-                    :type="isShowPassword ? 'text' : 'password'"
+                <ValidationProvider v-slot="v" rules="strong_password">
+                  <p class="control has-icons-left">
+                      <input
+                        class="input"
+                        :type="isShowPassword ? 'text' : 'password'"
+                        placeholder="Password"
+                        v-model="password"
+                        :disabled="!isEditing"
+                      >
+                      <span class="icon is-small is-left">
+                        <i class="fas fa-lock"></i>
+                      </span>
+                      <span
+                        :class="['fa fa-fw fa-eye field-icon', { 'fa-eye-slash': isShowPassword }]"
+                        @click="togglePasswordEye"
+                      >
+                      </span>
+                  </p>
+                  <Password
+                    v-show="isEditing"
                     placeholder="Password"
                     v-model="password"
-                    :disabled="!isEditing"
-                  >
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                  </span>
-                  <span
-                    :class="['fa fa-fw fa-eye field-icon', { 'fa-eye-slash': isShowPassword }]"
-                    @click="togglePasswordEye"
-                  >
-                  </span>
-                </p>
-                <Password
-                  placeholder="Password"
-                  v-model="password"
-                  :strength-meter-only="true"
-                />
+                    :strength-meter-only="true"
+                  />
+                  <p v-show="isEditing" class="help is-danger">{{ v.errors[0] }}</p>
+                </ValidationProvider>
               </div>
-              <div class="field is-grouped">
-                <div class="control">
-                    <button
-                      class="button is-link"
-                      @click="handleClick">
-                        {{ isEditing ? 'Submit' : 'Edit' }}
-                    </button>
-                </div>
+              <div>
+                  <button
+                    class="button is-link "
+                    @click="handleClick">
+                     <span class="icon is-small">
+                      <i :class="['fas', {'fa-check': isEditing}, { 'fa-edit': !isEditing }]"></i>
+                    </span>
+                    <span>{{ isEditing ? 'Submit' : 'Edit' }}</span>
+                  </button>
+                  <button
+                    v-if="isEditing"
+                    class="button is-warning"
+                    @click="handleCancel">
+                      <span class="icon is-small">
+                        <i class="fas fa-times"></i>
+                      </span>
+                      <span> Cancel </span>
+                  </button>
               </div>
           </div>
         </div>
@@ -88,43 +112,89 @@
   </div>
 </template>
 <script>
+import { ValidationProvider, extend } from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
 import Password from 'vue-password-strength-meter';
 import Gravatar from '../components/Gravatar';
+
+extend('required', {
+  ...required,
+  message: 'This field is required',
+});
+extend('email', {
+  ...email,
+  message: 'This email is invalid',
+});
+
+extend('strong_password', {
+  message: 'The password should contain at least: 1 uppercase letter, 1 lowercase letter, 1 number, and one special character (E.g. , . _ & ? etc)',
+  validate: (value) => {
+    const strongRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})');
+    return strongRegex.test(value) && value.length > 0;
+  },
+});
 
 export default {
   name: 'Register',
   components: {
+    ValidationProvider,
     Gravatar,
     Password,
   },
   data() {
     return {
-      first_name: 'DO MINH',
-      last_name: 'HAI',
-      email: 'haianhnc@gmail.com',
-      password: 'toHardtof4ind',
+      original: {
+        first_name: 'DO MINH',
+        last_name: 'HAI',
+        email: 'haianhnc@gmail.com',
+        password: 'toHardtof4ind',
+      },
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
       isEditing: false,
       isShowPassword: false,
     };
   },
   asyncData() {
-    console.log('WTF');
     // WIP fetching from API
-    // this.user = {
+    // this.original = {
     //   first_name: 'DO MINH',
     //   last_name: 'HAI',
     //   email: 'haianhnc@gmail.com',
     //   password: 'toHardtof4ind',
     // };
   },
+  mounted() {
+    this.first_name = this.original.first_name;
+    this.last_name = this.original.last_name;
+    this.email = this.original.email;
+    this.password = this.original.password;
+  },
   methods: {
     handleClick() {
       if (this.isEditing) {
         // save data
+        this.original = {
+          first_name: this.first_name,
+          last_name: this.last_name,
+          email: this.email,
+          password: this.password,
+        };
         this.isEditing = false;
       } else {
         this.isEditing = true;
       }
+      this.isShowPassword = false;
+    },
+    handleCancel() {
+      this.first_name = this.original.first_name;
+      this.last_name = this.original.last_name;
+      this.email = this.original.email;
+      this.password = this.original.password;
+      this.isEditing = false;
+      this.isShowPassword = false;
     },
     togglePasswordEye() {
       this.isShowPassword = !this.isShowPassword;
