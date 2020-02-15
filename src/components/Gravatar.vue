0641a510d8344661834e1a86d7d2b8cadc20711a
Do Minh Hai <haianhnc@gmail.com>
2020-02-16 02:28:17 +0700

    [Add] dummy Main page

---
+++ src/components/Gravatar.vue
@@ -0,0 +1,35 @@
+<template>
+  <img
+    :src="url"
+    :alt="alt"
+    @load="onLoad"
+    @error="onError"
+  />
+</template>
+
+<script>
+import md5 from 'md5';
+
+export default {
+  name: 'gravatar',
+  props: {
+    email: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    url() {
+      return ['https://www.gravatar.com/avatar/', md5(this.email.trim().toLowerCase()), 80, 'retro', 'g'].join('');
+    },
+  },
+  methods: {
+    onLoad(...args) {
+      this.$emit('load', ...args);
+    },
+    onError(...args) {
+      this.$emit('error', ...args);
+    },
+  },
+};
+</script>
---
+++ src/pages/Register.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <section class="section">
+      <Gravatar :email="user.email" />
+        <div class="columns">
+          <div class="column is-4 is-offset-4">
+              <div class="field">
+                <label class="label title-s1">Your Account Information</label>
+              </div>
+              <div class="columns">
+                <div class="column field">
+                  <div class="control">
+                      <input
+                        class="input"
+                        type="text"
+                        placeholder="First Name"
+                        :value="user.first_name"
+                        :disabled="!isEditing"
+                      >
+                  </div>
+                </div>
+                <div class="column field">
+                  <div class="control">
+                      <input
+                        class="input"
+                        type="text" placeholder="Last Name"
+                        :value="user.last_name"
+                        :disabled="!isEditing"
+                      >
+                  </div>
+                </div>
+              </div>
+              <div class="field">
+                <!-- <label class="label">Email</label> -->
+                <div class="control has-icons-left has-icons-right">
+                    <input
+                      class="input is-danger"
+                      type="email"
+                      placeholder="Email input"
+                      :value="user.email"
+                      :disabled="!isEditing"
+                    >
+                    <span class="icon is-small is-left">
+                    <i class="fa fa-envelope"></i>
+                    </span>
+                    <span class="icon is-small is-right">
+                    <i class="fa fa-warning"></i>
+                    </span>
+                </div>
+                <p class="help is-danger">This email is invalid</p>
+              </div>
+              <div class="field">
+                <p class="control has-icons-left">
+                  <input
+                    class="input"
+                    :type="isShowPassword ? 'text' : 'password'"
+                    placeholder="Password"
+                    :value="user.password"
+                    :disabled="!isEditing"
+                  >
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                  </span>
+                  <span
+                    :class="['fa fa-fw fa-eye field-icon', { 'fa-eye-slash': isShowPassword }]"
+                    @click="togglePasswordEye"
+                  >
+                  </span>
+                </p>
+              </div>
+              <div class="field is-grouped">
+                <div class="control">
+                    <button
+                      class="button is-link"
+                      @click="handleClick">
+                        {{ isEditing ? 'Submit' : 'Edit' }}
+                    </button>
+                </div>
+              </div>
+          </div>
+        </div>
+    </section>
+  </div>
+</template>
+<script>
+import Gravatar from '../components/Gravatar';
+
+export default {
+  name: 'Register',
+  components: {
+    Gravatar,
+  },
+  data() {
+    return {
+      user: {
+        first_name: 'DO MINH',
+        last_name: 'HAI',
+        email: 'haianhnc@gmail.com',
+        password: 'toHardtof4ind',
+      },
+      isEditing: false,
+      isShowPassword: false,
+    };
+  },
+  asyncData() {
+    console.log('WTF');
+    // WIP fetching from API
+    this.user = {
+      first_name: 'DO MINH',
+      last_name: 'HAI',
+      email: 'haianhnc@gmail.com',
+      password: 'toHardtof4ind',
+    };
+  },
+  methods: {
+    handleClick() {
+      if (this.isEditing) {
+        // save data
+        this.isEditing = false;
+      } else {
+        this.isEditing = true;
+      }
+    },
+    togglePasswordEye() {
+      this.isShowPassword = !this.isShowPassword;
+    },
+  },
+};
+</script>
+<style scoped>
+  .title-s1 {
+    font-size: 18px;
+  }
+  .field-icon {
+    float: right;
+    margin-top: -25px;
+    position: relative;
+    z-index: 2;
+  }
+</style>
